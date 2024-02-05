c4284c398bc0517c61f6e0ac6ceeb0e9f6099538
Axel Choteau <mail+git@axelc.fr>
2024-02-05 14:28:49 +0100

    Automatiquement renseigner l'auteur d'un post

--- api/src/Entity/Post.php
+++ api/src/Entity/Post.php
@@ -3,10 +3,18 @@
 namespace App\Entity;
 
 use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Delete;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\GetCollection;
+use ApiPlatform\Metadata\Patch;
+use ApiPlatform\Metadata\Put;
 use App\Repository\PostRepository;
+use App\State\AssociateUserToPostProcessor;
+use App\State\UserPasswordHasher;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Attribute\Groups;
 use Symfony\Component\Uid\Uuid;
 use Symfony\Component\Validator\Constraints\NotBlank;
 
@@ -14,7 +22,18 @@ use Symfony\Component\Validator\Constraints\NotBlank;
  * A user-generated post. Can contain text and/or files.
  */
 #[ORM\Entity(repositoryClass: PostRepository::class)]
-#[ApiResource]
+#[ApiResource(
+    operations: [
+        new GetCollection(),
+        new \ApiPlatform\Metadata\Post(processor: AssociateUserToPostProcessor::class),
+        new Get(),
+        new Put(),
+        new Patch(),
+        new Delete(),
+    ],
+    normalizationContext: ['groups' => ['post:read']],
+    denormalizationContext: ['groups' => ['post:write']],
+)]
 class Post
 {
     #[ORM\Id]
@@ -26,19 +45,24 @@ class Post
      */
     #[ORM\Column(length: 1000, nullable: true)]
     #[NotBlank]
+    #[Groups(['post:read', 'post:write'])]
     private ?string $textContent = null;
 
     /**
      * @var \DateTimeImmutable The original time of publication.
      */
     #[ORM\Column]
+    #[Groups(['post:read'])]
     private \DateTimeImmutable $postedAt;
 
     #[ORM\OneToMany(mappedBy: 'post', targetEntity: Attachment::class, orphanRemoval: true)]
+    #[Groups(['post:read'])]
     private Collection $attachments;
 
     #[ORM\ManyToOne(inversedBy: 'posts')]
     #[ORM\JoinColumn(nullable: false)]
+    #[Groups(['post:read'])]
+    // TODO : Ne pas demander author lors de la création (ça doit être automatique)
     private ?User $author = null;
 
     public function __construct()
